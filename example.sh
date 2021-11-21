4542a381171c25e7fed385192207c1a73869c0f9
Olavo <olavokruel@gmail.com>
2021-11-21 22:34:09 +0100

    Using script file into entrypoint

--- example.sh
+++ example.sh
@@ -1,28 +1,25 @@
-# Example on how to make the request using curl 
+# Example on how to make the request using curl
 # https://docs.github.com/en/rest/reference/repos#create-a-repository-dispatch-event
 #!Example 1
-curl --verbose \
+curl --verbose
 # The -X allows to choose the verb of the HTTP request
--X POST \
+-X POST
 # -d is the body of the request
 # It must minimun the "event_type" with the value specified in the action
 # It can also have the "client_payload" key to add some additional info
 -d '{ "event_type": "environments", "client_payload": { "env": "production" } }' \
--H 'Accept: application/vnd.github.v3+json' \
--H 'Content-type: application/json' \
+   -H 'Accept: application/vnd.github.v3+json' \
+   -H 'Content-type: application/json'
 # Github uses basic authorization with username and password to access its api.
--u "<userName>:<apiKey>" \
+-u "<userName>:<apiKey>"
 # Atention to all parts of the URL
-https://api.github.com/repos/<userName>/<repositoryName>/dispatches
-
+https://api.github.com/repos/ <userName >/ <repositoryName >/dispatches
 
 #! Example 2
 curl --verbose \
---url https://api.github.com/repos/<userName>/<repositoryName>/dispatches \
---request POST \
---header 'accept: application/vnd.github.v3+json' \
---header "content-type: application/json" \
---user "<userName>:<apiKey>" \
---data '{"event_type": "action", "client_payload": {"env": "production"}}'
-
-
+   --url https://api.github.com/repos/ \
+   --request POST \
+   --header 'accept: application/vnd.github.v3+json' \
+   --header "content-type: application/json" \
+   --user "<userName>:<GithubApiKey>" \
+   --data '{"event_type": "action", "client_payload": {"env": "production"}}' <userName >/ <repositoryName >/dispatches
---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo This is an arg from the github action flow --- $1
+echo Hello World
